b4f7ce47506fa4956e54b92bf0acab4f03efabe8
Shahriar-Utchas <123489314+Shahriar-Utchas@users.noreply.github.com>
2023-10-03 22:50:40 +0600

    create different shapes

--- create-shapes.cpp
+++ create-shapes.cpp
@@ -2,12 +2,11 @@
 #include <GL/glut.h>
 
 void display() {
-
+  
 glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 glClear(GL_COLOR_BUFFER_BIT);
 glOrtho(-5,5,-5,5,-5,5);
 glLineWidth(1);
-
 
 glBegin(GL_LINES);
 glColor3f(0.0f, 0.0f, 0.0f);
@@ -15,15 +14,11 @@ glVertex2f(-4.9f, 0.0f);    // x, y
 glVertex2f(4.9f, 0.0f);    // x, y
 glEnd();
 
-
-
 glBegin(GL_LINES);
 glColor3f(0.0f, 0.0f, 0.0f);
 glVertex2f(0.0f, 4.9f);    // x, y
 glVertex2f(0.0f, -4.9f);    // x, y
 glEnd();
-
-
 
 glBegin(GL_TRIANGLES);
 glColor3f(1.0f,1.0f,0.0f); //Yellow
@@ -31,8 +26,6 @@ glVertex2f(1.0f,-2.5f);
 glVertex2f(2.0f,-1.0f);
 glVertex2f(3.0f,-2.5f);
 glEnd();
-
-
 
 glBegin(GL_QUADS);
 glColor3f(1.0f,0.0f,0.0f); //Red
@@ -42,8 +35,6 @@ glVertex2f(-1.0f,3.0f);
 glVertex2f(-1.0f,1.0f);
 glEnd();
 
-
-
 glBegin(GL_QUADS);
 glColor3f(0.0f,1.0f,0.0f); //Green
 glVertex2f(1.0f,1.0f);
@@ -52,14 +43,12 @@ glVertex2f(3.0f,1.8f);
 glVertex2f(3.0f,1.0f);
 glEnd();
 
-
 glBegin(GL_TRIANGLES);
 glColor3f(0.0f,1.0f,0.0f); //Green
 glVertex2f(3.0f,0.6f);
 glVertex2f(3.0f,2.2f);
 glVertex2f(4.0f,1.4f);
 glEnd();
-
 
 glBegin(GL_TRIANGLES);
 glColor3f(0.5f,0.0f,1.0f); //Violet
